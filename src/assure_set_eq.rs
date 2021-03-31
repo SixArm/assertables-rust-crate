16da06110976f012c4f7c40e8f62031d33e5bcc7
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-03-30 20:37:36 -0700

    Add linked list examples

--- src/assure_set_eq.rs
+++ src/assure_set_eq.rs
@@ -8,14 +8,28 @@
 /// Like [`assure!`], this macro has a second form, 
 /// where a custom message can be provided.
 ///
-/// # Examples
+/// # Example with arrays
 ///
-/// ```
+/// ```rust
 /// # #[macro_use] extern crate assure; fn main() {
-/// let a = vec![1, 2, 3];
-/// let b = vec![3, 2, 1];
+/// let a = [1, 2];
+/// let b = [2, 1];
 /// assure_set_eq!(a, b);
-/// assure_set_eq!(a, b, "message");
+/// # }
+/// ```
+///
+/// # Example with linked lists
+///
+/// ```rust
+/// # #[macro_use] extern crate assure; fn main() {
+/// use std::collections::LinkedList;
+/// let mut a = LinkedList::new();
+/// a.push_back(1);
+/// a.push_back(2);
+/// let mut b = LinkedList::new();
+/// b.push_back(2);
+/// b.push_back(1);
+/// assure_set_eq!(a, b);
 /// # }
 /// ```
 #[macro_export]
@@ -50,9 +64,26 @@ macro_rules! assure_set_eq {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::LinkedList;
 
     #[test]
-    fn test_assure_set_eq_with_arity_2_return_ok() {
+    fn test_assure_set_eq_x_array_arity_2_return_ok() {
+        assert_eq!(
+            assure_set_eq!([1, 2], [1, 2]).unwrap(),
+            true
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_eq_x_array_arity_3_return_ok() {
+        assert_eq!(
+            assure_set_eq!([1, 2], [1, 2], "message").unwrap(),
+            true
+        )
+    } 
+
+    #[test]
+    fn test_assure_set_eq_x_vec_arity_2_return_ok() {
         assert_eq!(
             assure_set_eq!(vec![1, 2], vec![1, 2]).unwrap(),
             true
@@ -60,7 +91,7 @@ mod tests {
     } 
 
     #[test]
-    fn test_assure_set_eq_with_arity_3_return_ok() {
+    fn test_assure_set_eq_x_vec_arity_3_return_ok() {
         assert_eq!(
             assure_set_eq!(vec![1, 2], vec![1, 2], "message").unwrap(),
             true
@@ -68,7 +99,56 @@ mod tests {
     } 
 
     #[test]
-    fn test_assure_set_eq_with_arity_2_return_err() {
+    fn test_assure_set_eq_x_list_arity_2_return_ok() {
+        let mut a: LinkedList<u8> = LinkedList::new();
+        a.push_back(1);
+        a.push_back(2);
+        let mut b: LinkedList<u8> = LinkedList::new();
+        b.push_back(2);
+        b.push_back(1);
+        assert_eq!(
+            assure_set_eq!(a, b).unwrap(),
+            true
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_eq_x_list_arity_3_return_ok() {
+        let mut a: LinkedList<u8> = LinkedList::new();
+        a.push_back(1);
+        a.push_back(2);
+        let mut b: LinkedList<u8> = LinkedList::new();
+        b.push_back(2);
+        b.push_back(1);
+        assert_eq!(
+            assure_set_eq!(a, b, "message").unwrap(),
+            true
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_eq_x_array_arity_2_return_err() {
+        assert_eq!(
+            assure_set_eq!([1, 2], [3, 4])
+            .unwrap_err()
+            .chars()
+            .into_iter()
+            .filter(|x| !x.is_digit(10))
+            .collect::<String>(),
+            "assure_set_eq left:{, } right:{, }"
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_eq_x_array_arity_3_return_err() {
+        assert_eq!(
+            assure_set_eq!([1, 2], [3, 4], "message").unwrap_err(),
+            "message"
+        )
+    } 
+
+    #[test]
+    fn test_assure_set_eq_x_vec_arity_2_return_err() {
         assert_eq!(
             assure_set_eq!(vec![1, 2], vec![3, 4])
             .unwrap_err()
@@ -81,11 +161,43 @@ mod tests {
     } 
 
     #[test]
-    fn test_assure_set_eq_with_arity_3_return_err() {
+    fn test_assure_set_eq_x_vec_arity_3_return_err() {
         assert_eq!(
             assure_set_eq!(vec![1, 2], vec![3, 4], "message").unwrap_err(),
             "message"
         )
+    } 
+    #[test]
+    fn test_assure_set_eq_x_list_arity_2_return_err() {
+        let mut a: LinkedList<u8> = LinkedList::new();
+        a.push_back(1);
+        a.push_back(2);
+        let mut b: LinkedList<u8> = LinkedList::new();
+        b.push_back(3);
+        b.push_back(4);
+        assert_eq!(
+            assure_set_eq!(a, b)
+            .unwrap_err()
+            .chars()
+            .into_iter()
+            .filter(|x| !x.is_digit(10))
+            .collect::<String>(),
+            "assure_set_eq left:{, } right:{, }"
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_eq_x_list_arity_3_return_err() {
+        let mut a: LinkedList<u8> = LinkedList::new();
+        a.push_back(1);
+        a.push_back(2);
+        let mut b: LinkedList<u8> = LinkedList::new();
+        b.push_back(3);
+        b.push_back(4);
+        assert_eq!(
+            assure_set_eq!(a, b, "message").unwrap_err(),
+            "message"
+        );
     } 
 
 }
--- src/assure_set_ne.rs
+++ src/assure_set_ne.rs
@@ -8,14 +8,28 @@
 /// Like [`assert!`], this macro has a second form, where a custom
 /// panic message can be provided.
 ///
-/// # Examples
+/// # Example with arrays
 ///
-/// ```
+/// ```rust
 /// # #[macro_use] extern crate assure; fn main() {
-/// let a = vec![1, 2, 3];
-/// let b = vec![4, 5, 6];
+/// let a = [1, 2];
+/// let b = [3, 4];
 /// assure_set_ne!(a, b);
-/// assure_set_ne!(a, b, "message");
+/// # }
+/// ```
+///
+/// # Example with linked lists
+///
+/// ```rust
+/// # #[macro_use] extern crate assure; fn main() {
+/// use std::collections::LinkedList;
+/// let mut a = LinkedList::new();
+/// a.push_back(1);
+/// a.push_back(2);
+/// let mut b = LinkedList::new();
+/// b.push_back(3);
+/// b.push_back(4);
+/// assure_set_ne!(a, b);
 /// # }
 /// ```
 #[macro_export]
@@ -50,9 +64,26 @@ macro_rules! assure_set_ne {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::LinkedList;
 
     #[test]
-    fn test_assure_set_ne_with_arity_2_return_ok() {
+    fn test_assure_set_ne_x_array_arity_2_return_ok() {
+        assert_eq!(
+            assure_set_ne!([1, 2], [3, 4]).unwrap(),
+            true
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_ne_x_array_arity_3_return_ok() {
+        assert_eq!(
+            assure_set_ne!([1, 2], [3, 4], "message").unwrap(),
+            true
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_ne_x_vec_arity_2_return_ok() {
         assert_eq!(
             assure_set_ne!(vec![1, 2], vec![3, 4]).unwrap(),
             true
@@ -60,7 +91,7 @@ mod tests {
     } 
 
     #[test]
-    fn test_assure_set_ne_with_arity_3_return_ok() {
+    fn test_assure_set_ne_x_vec_arity_3_return_ok() {
         assert_eq!(
             assure_set_ne!(vec![1, 2], vec![3, 4], "message").unwrap(),
             true
@@ -68,7 +99,56 @@ mod tests {
     } 
 
     #[test]
-    fn test_assure_set_ne_with_arity_2_return_err() {
+    fn test_assure_set_ne_x_list_arity_2_return_ok() {
+        let mut a: LinkedList<u8> = LinkedList::new();
+        a.push_back(1);
+        a.push_back(2);
+        let mut b: LinkedList<u8> = LinkedList::new();
+        b.push_back(3);
+        b.push_back(4);    
+        assert_eq!(
+            assure_set_ne!(a, b).unwrap(),
+            true
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_ne_x_list_arity_3_return_ok() {
+        let mut a: LinkedList<u8> = LinkedList::new();
+        a.push_back(1);
+        a.push_back(2);
+        let mut b: LinkedList<u8> = LinkedList::new();
+        b.push_back(3);
+        b.push_back(4);    
+        assert_eq!(
+            assure_set_ne!(a, b, "message").unwrap(),
+            true
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_ne_x_array_arity_2_return_err() {
+        assert_eq!(
+            assure_set_ne!([1, 2], [1, 2])
+            .unwrap_err()
+            .chars()
+            .into_iter()
+            .filter(|x| !x.is_digit(10))
+            .collect::<String>(),
+            "assure_set_ne left:{, } right:{, }"
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_ne_x_array_arity_3_return_err() {
+        assert_eq!(
+            assure_set_ne!([1, 2], [1, 2], "message").unwrap_err(),
+            "message"
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_ne_x_vac_arity_2_return_err() {
         assert_eq!(
             assure_set_ne!(vec![1, 2], vec![1, 2])
             .unwrap_err()
@@ -81,9 +161,42 @@ mod tests {
     } 
 
     #[test]
-    fn test_assure_set_ne_with_arity_3_return_err() {
+    fn test_assure_set_ne_x_vac_arity_3_return_err() {
         assert_eq!(
             assure_set_ne!(vec![1, 2], vec![1, 2], "message").unwrap_err(),
+            "message"
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_ne_x_list_arity_2_return_err() {
+        let mut a: LinkedList<u8> = LinkedList::new();
+        a.push_back(1);
+        a.push_back(2);
+        let mut b: LinkedList<u8> = LinkedList::new();
+        b.push_back(2);
+        b.push_back(1);
+        assert_eq!(
+            assure_set_ne!(a, b)
+            .unwrap_err()
+            .chars()
+            .into_iter()
+            .filter(|x| !x.is_digit(10))
+            .collect::<String>(),
+            "assure_set_ne left:{, } right:{, }"
+        );
+    } 
+
+    #[test]
+    fn test_assure_set_ne_x_list_arity_3_return_err() {
+        let mut a: LinkedList<u8> = LinkedList::new();
+        a.push_back(1);
+        a.push_back(2);
+        let mut b: LinkedList<u8> = LinkedList::new();
+        b.push_back(2);
+        b.push_back(1);
+        assert_eq!(
+            assure_set_ne!(a, b, "message").unwrap_err(),
             "message"
         );
     } 
