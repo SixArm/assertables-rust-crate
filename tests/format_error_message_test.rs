1841934ab0b5c6e82cce251bb2bd7e6ffb443f76
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-02 23:54:08 -0700

    Add tests to format error message

--- tests/format_error_message_test.rs
+++ tests/format_error_message_test.rs
@@ -1,19 +1,44 @@
-use ::assure::*;
+use ::assertable::*;
 
-fn equality(a: i32, b: i32) -> Result<bool, String> {
-    assure_eq!(a, b, format!("error {} and {}", a, b))?;
+fn foo_assert(a: i32, b: i32) -> () {
+    assert_lt!(a, b, format!("message {} and {}", a, b))
+}
+
+fn foo_assume(a: i32, b: i32) -> Result<bool, String> {
+    assume_lt!(a, b, format!("message {} and {}", a, b))?;
     Ok(true)
 }
 
+fn foo_assure(a: i32, b: i32) -> Result<bool, String> {
+    return assure_lt!(a, b, format!("message {} and {}", a, b))
+}
 
 #[test]
-fn test_x_return_err() {
-    let a = 1;
-    let b = 2;
-    let x = equality(a, b);
-    assert!(x.is_err());
+#[should_panic (expected = "message 2 and 1")]
+fn test_foo_assert_x_failure() {
+    let a = 2;
+    let b = 1;
+    let _ = foo_assert(a, b);
+}
+
+#[test]
+fn test_foo_assume_x_failure() {
+    let a = 2;
+    let b = 1;
+    let x = foo_assume(a, b);
     assert_eq!(
         x.unwrap_err(),
-        "error 1 and 2"
+        "message 2 and 1"
+    );
+}
+
+#[test]
+fn test_foo_assure_x_failure() {
+    let a = 2;
+    let b = 1;
+    let x = foo_assure(a, b);
+    assert_eq!(
+        x.unwrap(),
+        false
     );
 }
