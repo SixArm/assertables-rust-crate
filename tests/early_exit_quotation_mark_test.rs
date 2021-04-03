4cdc2571546ff071fc231d979b6da4b888bc171a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-03 00:01:27 -0700

    Add tests to early axit quotation mark

---
+++ tests/early_exit_quotation_mark_test.rs
@@ -0,0 +1,29 @@
+use ::assertable::*;
+
+fn foo_assume(a: i32, b: i32) -> Result<i32, String> {
+    assume_lt!(a, b)?;
+    Ok(a)
+}
+
+
+#[test]
+fn test_x_success() {
+    let a = 1;
+    let b = 2;
+    let x = foo_assume(a, b);
+    assert_eq!(
+        x.unwrap(),
+        a
+    );
+}
+
+#[test]
+fn test_x_failure() {
+    let a = 2;
+    let b = 1;
+    let x = foo_assume(a, b);
+    assert_eq!(
+        x.unwrap_err(),
+        "assumption failed: `assume_lt(left, right)`\n  left: `2`\n right: `1`"
+    );
+}
