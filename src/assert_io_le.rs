c045c45172e946fe6d60e019bf1affca4ad3d93b
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-02 23:41:48 -0700

    Add assert_io_le, assert_io_lt, assert_io_ge, assert_io_gt

---
+++ src/assert_io_le.rs
@@ -0,0 +1,92 @@
+/// Assert one value is less than or equal to another value.
+///
+/// * When true, return `Ok(true)`.
+///
+/// * Otherwise, call [`panic!`] with a message and the values of the
+///   expressions with their debug representations.
+///
+/// # Examples
+///
+/// ```rust
+/// # #[macro_use] extern crate assertable; fn main() {
+/// assert_io_le!(1, 2);
+/// //-> ()
+/// # }
+/// ```
+///
+/// ```rust
+/// # #[macro_use] extern crate assertable; fn main() {
+/// // assert_io_le!(2, 1);
+/// //-> panic!("assertion failed: `assert_io_le(left, right)`\n  left: `2`,\n right: `1`")
+/// # }
+/// ```
+///
+/// This macro has a second form where a custom message can be provided.
+#[macro_export]
+macro_rules! assert_io_le {
+    ($left:expr, $right:expr $(,)?) => ({
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if (left_val <= right_val) {
+                    ()
+                } else {
+                    panic!("assertion failed: `assert_io_le(left, right)`\n  left: `{:?}`\n right: `{:?}`", $left, $right);
+                }
+            }
+        }
+    });
+    ($left:expr, $right:expr, $($arg:tt)+) => ({
+        match (&($left), &($right)) {
+            (left_val, right_val) => {
+                if (left_val <= right_val) {
+                    ()
+                } else {
+                    panic!("{:?}", $($arg)+)
+                }
+            }
+        }
+    });
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_assert_io_le_x_arity_2_success() {
+        let a = 1;
+        let b = 2;
+        let x = assert_io_le!(a, b);
+        assert_eq!(
+            x,
+            ()
+        );
+    }
+
+    #[test]
+    #[should_panic (expected = "assertion failed: `assert_io_le(left, right)`\n  left: `2`\n right: `1`")]
+    fn test_assert_io_le_x_arity_2_failure() {
+        let a = 2;
+        let b = 1;
+        assert_io_le!(a, b);
+    }
+
+    #[test]
+    fn test_assert_io_le_x_arity_3_success() {
+        let a = 1;
+        let b = 2;
+        let x = assert_io_le!(a, b, "message");
+        assert_eq!(
+            x,
+            ()
+        );
+    }
+
+    #[test]
+    #[should_panic (expected = "message")]
+    fn test_assert_io_le_x_arity_3_failure() {
+        let a = 2;
+        let b = 1;
+        assert_io_le!(a, b, "message");
+    }
+
+}
