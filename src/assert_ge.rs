581517d6ea54308b708eacfebae5d1d847afd8a2
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-02 23:16:33 -0700

    Add assert_le, assert_lt, assert_ge, assert_gt

---
+++ src/assert_ge.rs
@@ -0,0 +1,92 @@
+/// Assert one value is greater than or equal to another value.
+///
+/// * When true, return `()`.
+///
+/// * Otherwise, call [`panic!`] with a message and the values of the
+///   expressions with their debug representations.
+///
+/// # Examples
+///
+/// ```rust
+/// # #[macro_use] extern crate assertable; fn main() {
+/// assert_ge!(2, 1);
+/// //-> ()
+/// # }
+/// ```
+///
+/// ```rust
+/// # #[macro_use] extern crate assertable; fn main() {
+/// // assert_ge!(1, 2);
+/// //-> panic!("assertion failed: `assert_ge(left, right)`\n  left: `2`\n right: `1`")
+/// # }
+/// ```
+///
+/// This macro has a second form where a custom message can be provided.
+#[macro_export]
+macro_rules! assert_ge {
+    ($left:expr, $right:expr $(,)?) => ({
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if (left_val >= right_val) {
+                    ()
+                } else {
+                    panic!("assertion failed: `assert_ge(left, right)`\n  left: `{:?}`\n right: `{:?}`", $left, $right);
+                }
+            }
+        }
+    });
+    ($left:expr, $right:expr, $($arg:tt)+) => ({
+        match (&($left), &($right)) {
+            (left_val, right_val) => {
+                if (left_val >= right_val) {
+                    ()
+                } else {
+                    panic!("{:?}", $($arg)+)
+                }
+            }
+        }
+    });
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_assert_ge_x_arity_2_success() {
+        let a = 2;
+        let b = 1;
+        let x = assert_ge!(a, b);
+        assert_eq!(
+            x,
+            ()
+        );
+    }
+
+    #[test]
+    #[should_panic (expected = "assertion failed: `assert_ge(left, right)`\n  left: `1`\n right: `2`")]
+    fn test_assert_ge_x_arity_2_failure() {
+        let a = 1;
+        let b = 2;
+        let _ = assert_ge!(a, b);
+    }
+
+    #[test]
+    fn test_assert_ge_x_arity_3_success() {
+        let a = 2;
+        let b = 1;
+        let x = assert_ge!(a, b, "message");
+        assert_eq!(
+            x,
+            ()
+        );
+    }
+
+    #[test]
+    #[should_panic (expected = "message")]
+    fn test_assert_ge_x_arity_3_failure() {
+        let a = 1;
+        let b = 2;
+        let _ = assert_ge!(a, b, "message");
+    }
+
+}
---
+++ src/assert_lt.rs
@@ -0,0 +1,92 @@
+/// Assert one value is less than another value.
+///
+/// * When true, return `()`.
+///
+/// * Otherwise, call [`panic!`] with a message and the values of the
+///   expressions with their debug representations.
+///
+/// # Examples
+///
+/// ```rust
+/// # #[macro_use] extern crate assertable; fn main() {
+/// assert_lt!(1, 2);
+/// //-> ()
+/// # }
+/// ```
+///
+/// ```rust
+/// # #[macro_use] extern crate assertable; fn main() {
+/// // assert_lt!(2, 1);
+/// //-> panic!("assertion failed: `assert_lt(left, right)`\n  left: `2`\n right: `1`")
+/// # }
+/// ```
+///
+/// This macro has a second form where a custom message can be provided.
+#[macro_export]
+macro_rules! assert_lt {
+    ($left:expr, $right:expr $(,)?) => ({
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if (left_val < right_val) {
+                    ()
+                } else {
+                    panic!("assertion failed: `assert_lt(left, right)`\n  left: `{:?}`\n right: `{:?}`", $left, $right);
+                }
+            }
+        }
+    });
+    ($left:expr, $right:expr, $($arg:tt)+) => ({
+        match (&($left), &($right)) {
+            (left_val, right_val) => {
+                if (left_val < right_val) {
+                    ()
+                } else {
+                    panic!("{:?}", $($arg)+)
+                }
+            }
+        }
+    });
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_assert_lt_x_arity_2_success() {
+        let a = 1;
+        let b = 2;
+        let x = assert_lt!(a, b);
+        assert_eq!(
+            x,
+            ()
+        );
+    }
+
+    #[test]
+    #[should_panic (expected = "assertion failed: `assert_lt(left, right)`\n  left: `2`\n right: `1`")]
+    fn test_assert_lt_x_arity_2_failure() {
+        let a = 2;
+        let b = 1;
+        let _ = assert_lt!(a, b);
+    }
+
+    #[test]
+    fn test_assert_lt_x_arity_3_success() {
+        let a = 1;
+        let b = 2;
+        let x = assert_lt!(a, b, "message");
+        assert_eq!(
+            x,
+            ()
+        );
+    }
+
+    #[test]
+    #[should_panic (expected = "message")]
+    fn test_assert_lt_x_arity_3_failure() {
+        let a = 2;
+        let b = 1;
+        let _ = assert_lt!(a, b, "message");
+    }
+
+}
