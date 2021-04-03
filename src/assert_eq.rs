6d653101f03c9353246abb4980b10c2b5ad5e185
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-02 23:11:14 -0700

    Add assert_eq

---
+++ src/assert_eq.rs
@@ -0,0 +1,69 @@
+/// Assert two values are equal.
+///
+/// * When true, return `Ok(true)`.
+///
+/// * Otherwise, call [`panic!`] with a message and the values of the
+///   expressions with their debug representations.
+///
+/// # Examples
+///
+/// ```rust
+/// # #[macro_use] extern crate assertable; fn main() {
+/// assert_eq(1, 1);
+/// //-> Ok()
+/// # }
+/// ```
+///
+/// ```rust
+/// # #[macro_use] extern crate assertable; fn main() {
+/// // assert_eq!(1, 2);
+/// //-> panic!("assertion failed: `(left == right)`\n  left: `1`,\n right: `2`")
+/// # }
+/// ```
+///
+/// This macro has a second form where a custom message can be provided.
+
+// `assert_eq` macro is provided by Rust `std`.
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn test_assert_eq_x_arity_2_success() {
+        let a = 1;
+        let b = 1;
+        let x = assert_eq!(a, b);
+        assert_eq!(
+            x,
+            ()
+        );
+    }
+
+    #[test]
+    #[should_panic (expected = "assertion failed: `(left == right)`\n  left: `1`,\n right: `2`")]
+    fn test_assert_eq_x_arity_2_failure() {
+        let a = 1;
+        let b = 2;
+        assert_eq!(a, b);
+    }
+
+    #[test]
+    fn test_assert_eq_x_arity_3_success() {
+        let a = 1;
+        let b = 1;
+        let x = assert_eq!(a, b, "message");
+        assert_eq!(
+            x,
+            ()
+        );
+    }
+
+    #[test]
+    #[should_panic (expected = "message")]
+    fn test_assert_eq_x_arity_3_failure() {
+        let a = 1;
+        let b = 2;
+        assert_eq!(a, b, "message");
+    }
+
+}
