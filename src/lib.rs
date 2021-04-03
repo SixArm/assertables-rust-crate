b3b56a3634221533c6365306908a73c11b0576b1
Joel Parker Henderson <joel@joelparkerhenderson.com>
2021-04-02 23:43:13 -0700

    Add modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
-//! Assure: macros for Rust runtime checking
+//! assert-assume-assure: macros for Rust runtime checking
 //!
-//! This Rust crate provides the macro `assure!` and related macros.
-//!
-//! These are intentionally similar to the macro `assert!` and related macros.
+//! This Rust crate provides macros for Rust runtime checking.
+//! using `assert…!`, `assume…!`, `assure…!`, described below.
 //!
 //! Crate: [https://crates.io/crates/assure](https://crates.io/crates/assure)
 //!
@@ -13,104 +12,212 @@
 //!
 //! # Introduction
 //!
-//! The `assure` macros work like this:
+//! This Rust crate provides macros for Rust runtime checking,
+//! and each macro comes in three flavors:
 //!
-//! * `assure!(x)` returns `Result` with `Ok(x)` or `Err("assure")`.
+//! * `assert…!` returns `()` or calls [`panic!`]
 //!
-//! * `assure_eq(x, y)` returns `Result` with `Ok(x)` or `Err("assure_eq left:1 right:2"))`.
+//! * `assume…!` returns [`Result`] with `Ok(true)` or `Err(…)`
 //!
-//! For comparison `assert` macros work like this:
+//! * `assure…!` returns [`Result`] with `Ok(true)` or `Ok(false)` or `Err(…)`
 //!
-//! * `assert!(x)` returns successfully or calls `panic!`.
-//!
-//! * `assert_eq!(x, y)` returns successfully or calls `panic!`.
-//!
-//!
-//! # Return Ok or Err
-//!
-//! The `assure` macros return `Result` with either:
-//!
-//! * `Ok(…)` with the leftmost macro argument.
-//!
-//! * `Err(…)` with a generated error message intended for diagnostics.
-//!
-//! Example of `Ok`:
+//! Examples of `assert_lt!`:
 //!
 //! ```rust
-//! # #[macro_use] extern crate assure; fn main() {
-//! let a = 1;
-//! let b = 1;
-//! assure_eq!(a, b);
-//! //-> Ok(a)
+//! # #[macro_use] extern crate assertable; fn main() {
+//! assert_lt!(1, 2);
+//! //-> ()
 //! # }
 //! ```
 //!
-//! Example of `Err`:
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! // assert_lt!(2, 1);
+//! //-> panic!("assertion failed: `(left == right)`\n  left: `2`,\n right: `1`")
+//! # }
+//! ```
+//!
+//! Examples of `assume_lt!`:
 //!
 //! ```rust
-//! # #[macro_use] extern crate assure; fn main() {
-//! let a = 1;
-//! let b = 2;
-//! assure_eq!(a, b);
-//! //-> Err("assure_eq left:1 right:2")
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assume_lt!(1, 2);
+//! //-> Ok(true)
+//! # }
+//! ```
+//!
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assume_lt!(2, 1);
+//! //-> Err("assumption failed: `(left == right)`\n  left: `2`,\n right: `1`")
+//! # }
+//! ```
+//!
+//! Examples of `assure_lt!`:
+//!
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assure_lt!(1, 2);
+//! //-> Ok(true)
+//! # }
+//! ```
+//!
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assure_lt!(2, 1);
+//! //-> Ok(false)
+//! # }
+//! ```
+//!
+//! # Failure messages
+//!
+//! When a macro fails, it generates a failure message with the
+//! values of expressions with their debug representations, such as:
+//!
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! // assert_lt!(2, 1)
+//! //-> panic!("assertion failed: `(left == right)`\n  left: `2`,\n right: `1`")
+//! # }
+//! ```
+//!
+//! These macros have a second form where a custom message can be provided,
+//! such as:
+//!
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! // assert_lt!(2, 1, "my message here");
+//! //-> panic!("my message here")
 //! # }
 //! ```
 //!
 //!
-//! # Usage
+//! # Assert
 //!
-//! The `assure` macros can useful for checking with the `?` operator.
+//! The `assert…` macros can be useful with Rust testing,
+//! such as with macros that Rust `std` does not provide.
 //!
-//! This example function uses the `assure_gt!` macro, which means assure greater than:
+//! Example:
 //!
 //! ```rust
-//! # #[macro_use] extern crate assure; fn main() {
+//! # #[macro_use] extern crate assertable; fn main() {
 //! fn sum_positive_numbers(a: i32, b: i32) -> Result<i32, String> {
-//!     assure_gt!(a, 0)?;
-//!     assure_gt!(b, 0)?;
+//!     assert_lt!(0, a);
+//!     assert_lt!(0, b);
+//!     Ok(a + b)
+//! }
+//!
+//! sum_positive_numbers(1, 2);
+//! //-> 3
+//!
+//! // sum_positive_numbers(-1, -2);
+//! //-> panic!("assertion failed: `(left == right)`\n  left: `0`,\n right: `-1`")
+//! # }
+//! ```
+//!
+//! # Assume
+//!
+//! The `assume…` macros can be useful with the `?` operator, 
+//! such as with early exits in functions.
+//!
+//! Example:
+//!
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! fn sum_positive_numbers(a: i32, b: i32) -> Result<i32, String> {
+//!     assume_lt!(0, a)?;
+//!     assume_lt!(0, b)?;
 //!     Ok(a + b)
 //! }
 //!
 //! sum_positive_numbers(1, 2);
 //! //-> Ok(3)
 //!
-//! sum_positive_numbers(1, -2);
-//! //-> Err("assure_gt left:-2 right:0)
+//! sum_positive_numbers(-1, -2);
+//! //-> Err("assumption failed: `assume_lt(left, right)`\n  left: `0`,\n right: `-1`")
 //! # }
 //! ```
 //!
 //!
-//! # Custom error messages
+//! # Assure
 //!
-//! The `assure` macros generate a defult diagnostic error message such as:
+//! The `assure…` macros can be useful with chaining,
+//! such as with gate conditions in functions.
 //!
-//! * `assure_eq!(1, 2)` returns `Err("assure_eq left:1 right:2")`.
-//!
-//! The macros have a second form, where a custom error message can be provided
-//!  as the last argument:
-//!
-//! * `assure_eq!(1, 2, "message")` returns `Err("message")`.
-//!
-//! Example error message:
+//! Example:
 //!
 //! ```rust
-//! # #[macro_use] extern crate assure; fn main() {
+//! # #[macro_use] extern crate assertable; fn main() {
 //! fn sum_positive_numbers(a: i32, b: i32) -> Result<i32, String> {
-//!     assure_gt!(a, 0, "param 1 must be postive but is {}", a)?;
-//!     assure_gt!(b, 0, "param 2 must be postive but is {}", b)?;
-//!     Ok(a + b)
+//!     if assure_lt!(0, a).unwrap() && assure_lt!(0, b).unwrap() {
+//!         Ok(a + b)
+//!     } else {
+//!         Err(format!("please use positive numbers"))
+//!     }
 //! }
+//!
+//! sum_positive_numbers(1, 2);
+//! //-> Ok(3)
+//!
+//! sum_positive_numbers(-1, -2);
+//! //-> Err("must use postive numbers")
 //! # }
 //! ```
 //!
 //!
-//! # Macros for simple values
+//! # Macros for values
 //!
-//! Macro for truth checking:
+//! Examples:
+//!
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assume_lt!(1, 2);
+//! //-> Ok(true)
+//! # }
+//! ```
+//!
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assume_lt!(2, 1);
+//! //-> Err("assumption failed: `assert_lt(left, right)`\n  left: `2`\n right: `1`")
+//! # }
+//! ```
+//!
+//! `assert…` macros:
+//!
+//! * [`assert!`]`(a)`: assure `a` is true, provided by Rust `std`.
+//!
+//! * [`assert_eq!`]`(a, b)`: assert `a` is equal to `b`, provided by Rust `std`.
+//!
+//! * [`assert_ne!`]`(a, b)`: assert `a` is not equal to `b`, provided by Rust `std`.
+//!
+//! * [`assert_lt!`]`(a, b)`: assert `a` is less than `b`.
+//!
+//! * [`assert_le!`]`(a, b)`: assert `a` is less than or equal to `b`.
+//!
+//! * [`assert_gt!`]`(a, b)`: assert `a` is greater than `b`.
+//!
+//! * [`assert_ge!`]`(a, b)`: assert `a` is greater than or equal to `b`.
+//!
+//! `assume…` macros:
+//!
+//! * [`assume!`]`(a)`: assume `a` is true.
+//!
+//! * [`assume_eq!`]`(a, b)`: assume `a` is equal to `b`.
+//!
+//! * [`assume_ne!`]`(a, b)`: assume `a` is not equal to `b`.
+//!
+//! * [`assume_lt!`]`(a, b)`: assume `a` is less than `b`.
+//!
+//! * [`assume_le!`]`(a, b)`: assume `a` is less than or equal to `b`.
+//!
+//! * [`assume_gt!`]`(a, b)`: assume `a` is greater than `b`.
+//!
+//! * [`assume_ge!`]`(a, b)`: assume `a` is greater than or equal to `b`.
+//!
+//! `assure…` macros:
 //!
 //! * [`assure!`]`(a)`: assure `a` is true.
-//!
-//! Macros for value comparison:
 //!
 //! * [`assure_eq!`]`(a, b)`: assure `a` is equal to `b`.
 //!
@@ -127,75 +234,144 @@
 //!
 //! # Macros for set checking
 //!
-//! The `assure_set…!` macros help with comparison of set parameters, such as two arrays or two vectors. where the item order does not matter, and the item count does not matter.
+//! These macros help with comparison of set parameters,
+//! such as two arrays or two vectors. where the item order 
+//! does not matter, and the item count does not matter.
 //!
-//! * [`assure_set_eq`]`(a, b)`: assure the set `a` is equal to the set `b`.
-//!
-//! * [`assure_set_ne`]`(a, b)`: assure the set `a` is not equal to the set `b`.
-//!
-//! Example of `Ok`:
+//! Examples:
 //!
 //! ```rust
-//! # #[macro_use] extern crate assure; fn main() {
-//! let a = [1, 2];
-//! let b = [2, 1];
-//! assure_set_eq!(&a, &b);
-//! //-> Ok(&a)
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assume_set_eq!([1, 2], [2, 1]);
+//! //-> Ok(true)
 //! # }
 //! ```
 //!
-//! Example of `Err`:
-//!
 //! ```rust
-//! # #[macro_use] extern crate assure; fn main() {
-//! let a = [1, 2];
-//! let b = [3, 4];
-//! assure_set_eq!(&a, &b);
-//! //-> Err("assure_set_eq left:{1, 2} right:{3, 4}")
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assume_set_eq!([1, 2], [3, 4]);
+//! //-> Err("assertion failed: `assert_set_eq(left, right)`\n  left: `[1, 2]`\n right: `[3, 4]`")
 //! # }
 //! ```
+//!
+//! `assert_set…` macros:
+//!
+//! * [`assert_set_eq!`]`(a, b)`: assert the set `a` is equal to the set `b`.
+//!
+//! * [`assert_set_ne!`]`(a, b)`: assert the set `a` is not equal to the set `b`.
+//!
+//! `assume_set…` macros:
+//!
+//! * [`assume_set_eq!`]`(a, b)`: assume the set `a` is equal to the set `b`.
+//!
+//! * [`assume_set_ne!`]`(a, b)`: assume the set `a` is not equal to the set `b`.
+//!
+//! `assure_set…` macros:
+//!
+//! * [`assure_set_eq!`]`(a, b)`: assure the set `a` is equal to the set `b`.
+//!
+//! * [`assure_set_ne!`]`(a, b)`: assure the set `a` is not equal to the set `b`.
 //!
 //!
 //! # Macros for bag checking
 //!
-//! The `assure_bag…!` macros help with comparison of bag parameters, such as comparison of two arrays or two vectors, where the item order does not matter, and the item count does matter.
+//! Thes macros help with comparison of bag parameters, such as comparison of two arrays or two vectors, where the item order does not matter, and the item count does matter.
 //!
-//! * [`assure_bag_eq`]`(a, b)`: assure the bag `a` is equal to the bag `b`.
-//!
-//! * [`assure_bag_ne`]`(a, b)`: assure the bag `a` is not equal to the bag `b`.
-//!
-//! Example of `Ok`:
+//! Examples:
 //!
 //! ```rust
-//! # #[macro_use] extern crate assure; fn main() {
-//! let a = [1, 1];
-//! let b = [1, 1];
-//! assure_set_eq!(&a, &b);
-//! //-> Ok(&a)
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assume_bag_eq!([1, 1], [1, 1]);
+//! //-> Ok(true)
 //! # }
 //! ```
 //!
-//! Example of `Err`:
-//!
 //! ```rust
-//! # #[macro_use] extern crate assure; fn main() {
-//! let a = [1, 1];
-//! let b = [1, 1, 1];
-//! assure_set_eq!(&a, &b);
-//! //-> Err("assure_bag_eq left:{1: 2} right:{1: 3}")
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assume_bag_eq!([1, 1], [1, 1, 1]);
+//! //-> Err("assumption failed: `assume_bag_eq(left, right)`\n  left: `[1, 1]`\n right: `[1, 1, 1]`")]
 //! # }
 //! ```
+//!
+//! `assert_bag…` macros:
+//!
+//! * [`assert_bag_eq`]`(a, b)`: assert the bag `a` is equal to the bag `b`.
+//!
+//! * [`assert_bag_ne`]`(a, b)`: assert the bag `a` is not equal to the bag `b`.
+//!
+//! `assume_bag…` macros:
+//!
+//! * [`assume_bag_eq!`]`(a, b)`: assume the bag `a` is equal to the bag `b`.
+//!
+//! * [`assume_bag_ne!`]`(a, b)`: assume the bag `a` is not equal to the bag `b`.
+//!
+//! `assure_bag…` macros:
+//!
+//! * [`assure_bag_eq!`]`(a, b)`: assure the bag `a` is equal to the bag `b`.
+//!
+//! * [`assure_bag_ne!`]`(a, b)`: assure the bag `a` is not equal to the bag `b`.
 //!
 //!
 //! # Macros for IO-related checking
 //!
-//! The `assure_io…!` macros help with IO-related checking, such as comparison of files, streams, etc. These macros return a `Result` with `Ok(true)` or `Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, message))`.
+//! These macros help with IO-related checking, such as comparison of files, streams, etc. These macros return a `Result` with `Ok(true)` or `Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, message))`.
 //!
-//! Macro for truth checking:
+//! Examples:
+//!
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assume_io_lt!(1, 2);
+//! //-> Ok(true)
+//! # }
+//! ```
+//!
+//! ```rust
+//! # #[macro_use] extern crate assertable; fn main() {
+//! let x = assume_io_lt!(2, 1);
+//! //-> Err(
+//! //       std::io::Error::new(
+//! //           std::io::ErrorKind::InvalidInput, 
+//! //           "assumption failed: `assume_io_lt(left, right)`\n  left: `2`\n right: `1`")]
+//! //       )
+//! //   )
+//! # }
+//! ```
+//!
+//! `assert_io…` macros:
+//!
+//! * [`assert_io!`]`(a)`: assert `a` is true.
+//!
+//! * [`assert_io_eq!`]`(a, b)`: assert `a` is equal to `b`.
+//!
+//! * [`assert_io_ne!`]`(a, b)`: assert `a` is not equal to `b`.
+//!
+//! * [`assert_io_lt!`]`(a, b)`: assert `a` is less than `b`.
+//!
+//! * [`assert_io_le!`]`(a, b)`: assert `a` is less than or equal to `b`.
+//!
+//! * [`assert_io_gt!`]`(a, b)`: assert `a` is greater than `b`.
+//!
+//! * [`assert_io_ge!`]`(a, b)`: assert `a` is greater than or equal to `b`.
+//!
+//! `assume_io…` macros:
+//!
+//! * [`assume_io!`]`(a)`: assume `a` is true.
+//!
+//! * [`assume_io_eq!`]`(a, b)`: assume `a` is equal to `b`.
+//!
+//! * [`assume_io_ne!`]`(a, b)`: assume `a` is not equal to `b`.
+//!
+//! * [`assume_io_lt!`]`(a, b)`: assume `a` is less than `b`.
+//!
+//! * [`assume_io_le!`]`(a, b)`: assume `a` is less than or equal to `b`.
+//!
+//! * [`assume_io_gt!`]`(a, b)`: assume `a` is greater than `b`.
+//!
+//! * [`assume_io_ge!`]`(a, b)`: assume `a` is greater than or equal to `b`.
+//!
+//! `assure_io…` macros:
 //!
 //! * [`assure_io!`]`(a)`: assure `a` is true.
-//!
-//! Macros for value comparison:
 //!
 //! * [`assure_io_eq!`]`(a, b)`: assure `a` is equal to `b`.
 //!
@@ -209,35 +385,70 @@
 //!
 //! * [`assure_io_ge!`]`(a, b)`: assure `a` is greater than or equal to `b`.
 //!
-//! Example of `Ok`:
-//!
-//! ```rust
-//! # #[macro_use] extern crate assure; fn main() {
-//! let a = 1;
-//! let b = 1;
-//! assure_io_eq!(a, b);
-//! //-> Ok(a)
-//! # }
-//! ```
-//!
-//! Example of `Err`:
-//!
-//! ```rust
-//! # #[macro_use] extern crate assure; fn main() {
-//! let a = 1;
-//! let b = 2;
-//! assure_io_eq!(a, b);
-//! //-> Err(
-//! //       std::io::Error::new(
-//! //           std::io::ErrorKind::InvalidInput, 
-//! //           "assure_io_eq left:1 right:2"
-//! //       )
-//! //   )
-//! # }
 //! ```
 
+// Assert truth
+pub mod assert; // condition (provided by Rust `std`)
+
+// Assert value comparison
+pub mod assert_eq; // equal (provided by Rust `std`)
+pub mod assert_ne; // not equal (provided by Rust `std`)
+pub mod assert_lt; // less than
+pub mod assert_le; // less than or equal to
+pub mod assert_gt; // greater than
+pub mod assert_ge; // greater than or equal to
+
+// Assert iterator-related set-based comparison
+pub mod assert_set_eq; // equal
+pub mod assert_set_ne; // not equal
+
+// Assert iterator-related bag-based comparison
+pub mod assert_bag_eq; // equal
+pub mod assert_bag_ne; // not equal
+
+// Assert IO-related truth, which can return Err(std:io:Error(…))
+//pub mod assert_io;
+
+// Assert IO-related comparison, which can return Err(std:io:Error(…))
+pub mod assert_io_eq; // equal
+pub mod assert_io_ne; // not equal
+pub mod assert_io_lt; // less than
+pub mod assert_io_le; // less than or equal to
+pub mod assert_io_gt; // greater than
+pub mod assert_io_ge; // greater than or equal to
+
+// Assume truth
+pub mod assume; // condition
+
+// Assume value comparison
+pub mod assume_eq; // equal
+pub mod assume_ne; // not equal
+pub mod assume_lt; // less than
+pub mod assume_le; // less than or equal to
+pub mod assume_gt; // greater than
+pub mod assume_ge; // greater than or equal to
+
+// Assume iterator-related set-based comparison
+pub mod assume_set_eq; // equal
+pub mod assume_set_ne; // not equal
+
+// Assume iterator-related bag-based comparison
+pub mod assume_bag_eq; // equal
+pub mod assume_bag_ne; // not equal
+
+// Assume IO-related truth, which can return Err(std:io:Error(…))
+pub mod assume_io;
+
+// Assume IO-related comparison, which can return Err(std:io:Error(…))
+pub mod assume_io_eq; // equal
+pub mod assume_io_ne; // not equal
+pub mod assume_io_lt; // less than
+pub mod assume_io_le; // less than or equal to
+pub mod assume_io_gt; // greater than
+pub mod assume_io_ge; // greater than or equal to
+
 // Assure truth
-pub mod assure;
+pub mod assure; // condition
 
 // Assure value comparison
 pub mod assure_eq; // equal
