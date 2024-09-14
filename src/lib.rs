36c739e6f67c4671238bfa9a414a41d08b26fc5a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-09-14 07:25:40 +0200

    Bump version date

--- src/lib.rs
+++ src/lib.rs
@@ -204,7 +204,7 @@
 //! * Package: assertables-rust-crate
 //! * Version: 8.5.0
 //! * Created: 2021-03-30T15:47:49Z
-//! * Updated: 2024-09-12T18:16:41Z
+//! * Updated: 2024-09-14T05:24:55Z
 //! * License: MIT or Apache-2.0 or GPL-2.0 or GPL-3.0 or contact us for more
 //! * Contact: Joel Parker Henderson (joel@sixarm.com)
 
