bcbc7c2ef82dedfbbd771f7d31d934b872f2fe27
Joel Parker Henderson <joel@joelparkerhenderson.com>
2024-10-02 14:01:59 +0200

    Refactor Some/None to modules

---
+++ src/assert_some/mod.rs
@@ -0,0 +1,46 @@
+//! Assert macros for Some(_) items.
+//!
+//! These macros help compare Some(…) items, such as `std::Option::Some` or similar.
+//!
+//! Assert expression is Some(_):
+//! 
+//! * [`assert_some!(a)`](macro@crate::assert_some) 
+//!   ≈ a is Some(_)
+//! 
+//! Compare Some(…) to another Some(…):
+//! 
+//! * [`assert_some_eq!(a, b)`](macro@crate::assert_some_eq) 
+//!   ≈ (a ⇒ Some(a̅) ⇒ a̅) = (b ⇒ Some(b̅) ⇒ b̅)
+//!
+//! * [`assert_some_ne!(a, b)`](macro@crate::assert_some_ne) 
+//!   ≈ (a ⇒ Some(a̅) ⇒ a̅) ≠ (b ⇒ Some(b̅) ⇒ b̅)
+//!
+//! Compare Some(…) to an expression:
+//! 
+//! * [`assert_some_eq_expr!(a, expr)`](macro@crate::assert_some_eq_expr) 
+//!   ≈ (a ⇒ Some(a̅) ⇒ a̅) = b
+//!
+//! * [`assert_some_ne_expr!(a, b)`](macro@crate::assert_some_ne_expr) 
+//!   ≈ (a ⇒ Some(a̅) ⇒ a̅) ≠ b
+//!
+//! # Example
+//!
+//! ```rust
+//! use assertables::*;
+//! # fn main() {
+//! let a: Option<i8> = Some(1);
+//! let b: Option<i8> = Some(1);
+//! assert_some_eq!(a, b);
+//! # }
+//! ```
+
+// Verify Some(_)
+pub mod assert_some;
+
+// Compare with other
+pub mod assert_some_eq;
+pub mod assert_some_ne;
+
+// Compare with expression
+pub mod assert_some_eq_expr;
+pub mod assert_some_ne_expr;
