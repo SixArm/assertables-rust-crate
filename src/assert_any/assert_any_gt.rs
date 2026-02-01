b70eedb45b7837a5884aefcee8f49b6f078b8b72
tgmof <>
2026-02-01 15:45:39 +0100

    Implement suggestions

---
+++ src/assert_any/assert_any_gt.rs
@@ -0,0 +1,293 @@
+//! Assert an iter is greater than another.
+//!
+//! Pseudocode:<br>
+//! (collection into iter) > item
+//!
+//! # Example
+//!
+//! ```rust
+//! use assertables::*;
+//!
+//! let a = [3, 4];
+//! let b = 4;
+//! assert_any_gt!(&a, &b);
+//! ```
+//!
+//! This implementation uses [`::std::iter::Iterator`](https://doc.rust-lang.org/std/iter/trait.Iterator.html).
+//!
+//! # Module macros
+//!
+//! * [`assert_any_gt`](macro@crate::assert_any_gt)
+//! * [`assert_any_gt_as_result`](macro@crate::assert_any_gt_as_result)
+//! * [`debug_assert_any_gt`](macro@crate::debug_assert_any_gt)
+
+/// Assert an iterable is greater than another.
+///
+/// Pseudocode:<br>
+/// (collection into iter) > item
+///
+/// * If true, return Result `Ok(())`.
+///
+/// * Otherwise, return Result `Err(message)`.
+///
+/// This macro is useful for runtime checks, such as checking parameters,
+/// or sanitizing inputs, or handling different results in different ways.
+///
+/// This implementation uses [`::std::iter::Iterator`](https://doc.rust-lang.org/std/iter/trait.Iterator.html).
+///
+/// # Module macros
+///
+/// * [`assert_any_gt`](macro@crate::assert_any_gt)
+/// * [`assert_any_gt_as_result`](macro@crate::assert_any_gt_as_result)
+/// * [`debug_assert_any_gt`](macro@crate::debug_assert_any_gt)
+///
+#[macro_export]
+macro_rules! assert_any_gt_as_result {
+    ($a_collection:expr, $b_item:expr $(,)?) => {
+        match ($a_collection, $b_item) {
+            (a_collection, b_item) => {
+                let mut a = a_collection.into_iter();
+                if a.any(|x| x > b_item) {
+                    Ok(())
+                } else {
+                    Err(format!(
+                        concat!(
+                            "assertion failed: `assert_any_gt!(a_collection, b_item)`\n",
+                            "https://docs.rs/assertables/9.8.4/assertables/macro.assert_any_gt.html\n",
+                            " a label: `{}`,\n",
+                            " a debug: `{:?}`,\n",
+                            " b label: `{}`,\n",
+                            " b debug: `{:?}`"
+                        ),
+                        stringify!($a_collection),
+                        a_collection,
+                        stringify!($b_item),
+                        b_item
+                    ))
+                }
+            }
+        }
+    };
+}
+
+#[cfg(test)]
+mod test_assert_any_gt_as_result {
+    // use std::sync::Once;
+
+    #[test]
+    fn gt() {
+        let a = [3, 7];
+        let b = 4;
+        for _ in 0..1 {
+            let actual = assert_any_gt_as_result!(&a, &b);
+            assert_eq!(actual.unwrap(), ());
+        }
+    }
+
+    #[test]
+    fn eq() {
+        let a = [1, 2];
+        let b = 2;
+        let actual = assert_any_gt_as_result!(&a, &b);
+        let message = concat!(
+            "assertion failed: `assert_any_gt!(a_collection, b_item)`\n",
+            "https://docs.rs/assertables/9.8.4/assertables/macro.assert_any_gt.html\n",
+            " a label: `&a`,\n",
+            " a debug: `[1, 2]`,\n",
+            " b label: `&b`,\n",
+            " b debug: `2`"
+        );
+        assert_eq!(actual.unwrap_err(), message);
+    }
+
+    #[test]
+    fn lt() {
+        let a = [1, 2];
+        let b = 0;
+        let actual = assert_any_gt_as_result!(&a, &b);
+        let message = concat!(
+            "assertion failed: `assert_any_gt!(a_collection, b_item)`\n",
+            "https://docs.rs/assertables/9.8.4/assertables/macro.assert_any_gt.html\n",
+            " a label: `&a`,\n",
+            " a debug: `[1, 2]`,\n",
+            " b label: `&b`,\n",
+            " b debug: `0`"
+        );
+        assert_eq!(actual.unwrap_err(), message);
+    }
+}
+
+/// Assert an iterable is greater than another.
+///
+/// Pseudocode:<br>
+/// (collection into iter) > item
+///
+/// * If true, return `()`.
+///
+/// * Otherwise, call [`panic!`] with a message and the values of the
+///   expressions with their debug representations.
+///
+/// # Examples
+///
+/// ```rust
+/// use assertables::*;
+/// # use std::panic;
+///
+/// # fn main() {
+/// let a = [3, 4];
+/// let b = 5;
+/// assert_any_gt!(&a, &b);
+///
+/// # let result = panic::catch_unwind(|| {
+/// // This will panic
+/// let a = [1, 2];
+/// let b = 2;
+/// assert_any_gt!(&a, &b);
+/// # });
+/// // assertion failed: `assert_any_gt!(a_collection, b_item)`
+/// // https://docs.rs/assertables/…/assertables/macro.assert_any_gt.html
+/// //  a label: `&a`,
+/// //  a debug: `[1, 2]`,
+/// //  b label: `&b`,
+/// //  b debug: `2`
+/// # let actual = result.unwrap_err().downcast::<String>().unwrap().to_string();
+/// # let message = concat!(
+/// #     "assertion failed: `assert_any_gt!(a_collection, b_item)`\n",
+/// #     "https://docs.rs/assertables/9.8.4/assertables/macro.assert_any_gt.html\n",
+/// #     " a label: `&a`,\n",
+/// #     " a debug: `[1, 2]`,\n",
+/// #     " b label: `&b`,\n",
+/// #     " b debug: `2`",
+/// # );
+/// # assert_eq!(actual, message);
+/// # }
+/// ```
+///
+/// This implementation uses [`::std::iter::Iterator`](https://doc.rust-lang.org/std/iter/trait.Iterator.html).
+///
+/// # Module macros
+///
+/// * [`assert_any_gt`](macro@crate::assert_any_gt)
+/// * [`assert_any_gt_as_result`](macro@crate::assert_any_gt_as_result)
+/// * [`debug_assert_any_gt`](macro@crate::debug_assert_any_gt)
+///
+#[macro_export]
+macro_rules! assert_any_gt {
+    ($a_collection:expr, $b_item:expr $(,)?) => {
+        match $crate::assert_any_gt_as_result!($a_collection, $b_item) {
+            Ok(()) => (),
+            Err(err) => panic!("{}", err),
+        }
+    };
+    ($a_collection:expr, $b_item:expr, $($message:tt)+) => {
+        match $crate::assert_any_gt_as_result!($a_collection, $b_item) {
+            Ok(()) => (),
+            Err(err) => panic!("{}\n{}", format_args!($($message)+), err),
+        }
+    };
+}
+
+#[cfg(test)]
+mod test_assert_any_gt {
+    use std::panic;
+
+    #[test]
+    fn gt() {
+        let a = [3, 7];
+        let b = 4;
+        for _ in 0..1 {
+            let actual = assert_any_gt!(&a, &b);
+            assert_eq!(actual, ());
+        }
+    }
+
+    #[test]
+    fn eq() {
+        let a = [1, 2];
+        let b = 2;
+        let result = panic::catch_unwind(|| {
+            let _actual = assert_any_gt!(&a, &b);
+        });
+        let message = concat!(
+            "assertion failed: `assert_any_gt!(a_collection, b_item)`\n",
+            "https://docs.rs/assertables/9.8.4/assertables/macro.assert_any_gt.html\n",
+            " a label: `&a`,\n",
+            " a debug: `[1, 2]`,\n",
+            " b label: `&b`,\n",
+            " b debug: `2`"
+        );
+        assert_eq!(
+            result
+                .unwrap_err()
+                .downcast::<String>()
+                .unwrap()
+                .to_string(),
+            message
+        );
+    }
+
+    #[test]
+    fn lt() {
+        let a = [1, 2];
+        let b = 0;
+        let result = panic::catch_unwind(|| {
+            let _actual = assert_any_gt!(&a, &b);
+        });
+        let message = concat!(
+            "assertion failed: `assert_any_gt!(a_collection, b_item)`\n",
+            "https://docs.rs/assertables/9.8.4/assertables/macro.assert_any_gt.html\n",
+            " a label: `&a`,\n",
+            " a debug: `[1, 2]`,\n",
+            " b label: `&b`,\n",
+            " b debug: `0`"
+        );
+        assert_eq!(
+            result
+                .unwrap_err()
+                .downcast::<String>()
+                .unwrap()
+                .to_string(),
+            message
+        );
+    }
+}
+
+/// Assert an iterable is greater than another.
+///
+/// Pseudocode:<br>
+/// (collection into iter) > item
+///
+/// This macro provides the same statements as [`assert_any_gt`](macro.assert_any_gt.html),
+/// except this macro's statements are only enabled in non-optimized
+/// builds by default. An optimized build will not execute this macro's
+/// statements unless `-C debug-assertions` is passed to the compiler.
+///
+/// This macro is useful for checks that are too expensive to be present
+/// in a release build but may be helpful during development.
+///
+/// The result of expanding this macro is always type checked.
+///
+/// An unchecked assertion allows a program in an inconsistent state to
+/// keep running, which might have unexpected consequences but does not
+/// introduce unsafety as long as this only happens in safe code. The
+/// performance cost of assertions, however, is not measurable in general.
+/// Replacing `assert*!` with `debug_assert*!` is thus only encouraged
+/// after thorough profiling, and more importantly, only in safe code!
+///
+/// This macro is intended to work in a similar way to
+/// [`::std::debug_assert`](https://doc.rust-lang.org/std/macro.debug_assert.html).
+///
+/// # Module macros
+///
+/// * [`assert_any_gt`](macro@crate::assert_any_gt)
+/// * [`assert_any_gt`](macro@crate::assert_any_gt)
+/// * [`debug_assert_any_gt`](macro@crate::debug_assert_any_gt)
+///
+#[macro_export]
+macro_rules! debug_assert_any_gt {
+    ($($arg:tt)*) => {
+        if $crate::cfg!(debug_assertions) {
+            $crate::assert_any_gt!($($arg)*);
+        }
+    };
+}
---
+++ src/assert_any/mod.rs
@@ -0,0 +1,31 @@
+//! Assert for comparing an iter collection with a single element.
+//!
+//! These macros help with comparison of iter parameters, such as a collection of struct
+//! and a single struct.
+//! These macros convert each input using the std::iter::Iterator trait.
+//!
+//! * [`assert_any_eq!(collection, item)`](macro@crate::assert_any_eq) ≈ iter a contains b
+//! * [`assert_any_ne!(collection, item)`](macro@crate::assert_any_ne) ≈ iter a does not contain b
+//! * [`assert_any_lt!(collection, item)`](macro@crate::assert_any_gt) ≈ iter a contains ≥ one element < b
+//! * [`assert_any_le!(collection, item)`](macro@crate::assert_any_gt) ≈ iter a contains ≥ one element ≤ b
+//! * [`assert_any_gt!(collection, item)`](macro@crate::assert_any_gt) ≈ iter a contains ≥ one element > b
+//! * [`assert_any_ge!(collection, item)`](macro@crate::assert_any_ge) ≈ iter a contains ≥ one element ≥ b
+//!
+//! # Example
+//!
+//! ```rust
+//! use assertables::*;
+//!
+//! let a = [1, 2];
+//! let b = [1, 2];
+//! assert_any_eq!(&a, &b);
+//! ```
+
+// Comparisons
+pub mod assert_any;
+pub mod assert_any_eq;
+pub mod assert_any_ge;
+pub mod assert_any_gt;
+pub mod assert_any_le;
+pub mod assert_any_lt;
+pub mod assert_any_ne;
